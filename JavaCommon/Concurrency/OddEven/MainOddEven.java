43d1e07a0d781cd14feb4d647423e79a1bc60172
gokul <rajgokul9909@gmail.com>
2026-07-14 02:06:31 +0530

    Odd even

--- JavaCommon/Concurrency/OddEven/MainOddEven.java
+++ JavaCommon/Concurrency/OddEven/MainOddEven.java
@@ -1,4 +1,52 @@
 package Concurrency.OddEven;
 
 public class MainOddEven {
+
+    public static void main(String... args)
+    {
+        OddEvenWorker worker = new OddEvenWorker(7);
+        Thread  thread = new Thread(()->{
+
+                try {
+                    worker.printOdd();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+        });
+
+        Thread  thread3 = new Thread(()->{
+
+
+                try {
+                    worker.printEven();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+        });
+
+
+        Thread  thread2 = new Thread(()->{
+
+
+                try {
+                    worker.printZero();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+        });
+
+
+
+        thread.start();
+        thread2.start();
+        thread3.start();
+
+
+
+    }
 }
--- JavaCommon/Concurrency/OddEven/OddEvenWorker.java
+++ JavaCommon/Concurrency/OddEven/OddEvenWorker.java
@@ -1,4 +1,57 @@
 package Concurrency.OddEven;
 
+import java.util.concurrent.Semaphore;
+
 public class OddEvenWorker {
+
+    int number=1;
+    boolean odd = true;
+
+    Semaphore oddSemaphore = new Semaphore(0);
+    Semaphore evenSemaphore = new Semaphore(0);
+    Semaphore zeroSemaphore = new Semaphore(1);
+
+
+    OddEvenWorker(int number)
+    {
+
+        this.number = number;
+    }
+
+    public void printOdd() throws InterruptedException {
+
+        for(int i=1;i<=number;i=i+2) {
+            oddSemaphore.acquire(1);
+            System.out.println(i);
+            zeroSemaphore.release(1);
+        }
+
+
+
+    }
+
+    public void printEven() throws InterruptedException {
+        for(int i=2;i<=number;i=i+2){
+            evenSemaphore.acquire(1);
+            System.out.println(i);
+            zeroSemaphore.release(1);
+
+        }
+    }
+
+    public void printZero() throws InterruptedException {
+        for(int i=0;i<=number;i++)
+        {
+            zeroSemaphore.acquire(1);
+            System.out.println(0);
+            if(odd) {
+                oddSemaphore.release(1);
+            }
+            else{
+                evenSemaphore.release(1);
+            }
+            odd = !odd;
+
+        }
+    }
 }
